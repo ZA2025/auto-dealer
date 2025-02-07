d7619b9928c24f27e072c8bee0b0d023bbacb5d1
ZA2025 <zainabal2025@gmail.com>
2025-02-07 22:12:54 +0000

    fix subscribe component

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import styles from './Products.module.scss';
+import styles from './Subscribe.module.scss';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
